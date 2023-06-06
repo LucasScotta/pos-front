ead2b5ddeba794f5ea7df1c238a2efd64651aeed
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-06 18:32:25 -0300

    Auth guard using Selector again

--- src/Guards/AdminGuard/AdminGuard.tsx
+++ src/Guards/AdminGuard/AdminGuard.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { path, roles } from "../../helper"
-import { useUser } from "../../Hooks"
+import { useSelector } from "react-redux"
+import { AppStore } from "../../Models"
 
 export const AdminGuard = () => {
-    const { user } = useUser()
+    const { user } = useSelector((store: AppStore) => store)
     if (!user) return <Navigate to={path.HOME} />
     const { rol } = user
     return rol === roles.ADMIN ? <Outlet /> : <Navigate to={path.DASHBOARD} />
