5f38d9b7105cac4aea648968860801f29b1f3838
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-06 15:50:39 -0300

    Auth guard username conditional fixed

--- src/Guards/Auth/Auth.guard.tsx
+++ src/Guards/Auth/Auth.guard.tsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { path } from "../../helper"
-import { useUser } from "../../Hooks"
+import { useSelector } from "react-redux"
+import { AppStore } from "../../Models"
 
 export const AuthGuard = () => {
-    const { user } = useUser()
+    const { user } = useSelector((store: AppStore) => store)
     return !!user && !!user.username && <Outlet /> || <Navigate to={path.HOME} />
 }
