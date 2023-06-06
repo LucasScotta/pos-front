89bbcaf4498b8f70ad016e7df39f88686c2da380
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-06 15:51:12 -0300

    dispatch added to useUser custom hook

--- src/Hooks/useUser/useUser.ts
+++ src/Hooks/useUser/useUser.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { AppStore, UserInfo } from "../../Models"
+import { useDispatch } from "react-redux"
 
 export const useUser = () => {
     const userState = useSelector((store: AppStore) => store.user)
+    const dispatch = useDispatch()
+
     const [user, setUser] = useState<UserInfo>()
-    useEffect(() => {
-        const { username } = userState
-        if (!username) return
+    const updatedUser = () => setUser(userState)
+    useEffect(updatedUser, [])
+    useEffect(updatedUser, [userState])
 
-        setUser(userState)
-    }, [userState])
-
-    return { user }
+    return { user, dispatch }
 }
