29e918f00a61f1a4b21e8b1b897fe763140cf72d
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-08 16:17:13 -0300

    config updated

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+const API_PATH = process.env.API_PATH || 'http://localhost'
+const IO_PORT = process.env.IO_PORT || '1234'
+const API_PORT = process.env.AIP_PORT || '8080'
+
+export { API_PATH, IO_PORT, API_PORT }
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      API_PATH: process.env.API_PATH || 'http://localhost',
+      IO_PORT: process.env.IO_PORT || '1234',
+      API_PORT: process.env || '8080'
+    },
+  }
 })
