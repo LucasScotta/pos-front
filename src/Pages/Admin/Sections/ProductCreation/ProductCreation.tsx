d68ca539d25211eb836e7ec96c0e25bca27b33eb
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-08 12:04:01 -0300

    product creation form implemented Form Component

--- src/Pages/Admin/Sections/ProductCreation/ProductCreation.tsx
+++ src/Pages/Admin/Sections/ProductCreation/ProductCreation.tsx
@@ -1,12 +1,41 @@
-import { FormEvent } from "react"
+import { FormEvent, useState } from "react"
+import { Form } from "../../../../Components"
+import { registerProductInputs } from "./registerProductInputs"
 
 export const ProductCreation = () => {
-    const createProduct = (e: FormEvent) => e.preventDefault()
+    const [error, setError] = useState('')
+    const [timeError, setTimeError] = useState<number>()
+    const handleError = (err: string) => {
+        clearError()
+        setError(err)
+        const timeOut = setTimeout(() => setError(''), 3000)
+        setTimeError(timeOut)
+    }
+    const clearError = () => clearInterval(timeError)
+    const createProduct = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
 
-    return <form onClick={createProduct} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h3>Register new product</h3>
-        <input type="text" name="name" required defaultValue="" />
-        <input type="number" name="price" required defaultValue="" />
-        <button>Create Product</button>
-    </form>
+        const form = e.currentTarget
+        const formData = new FormData(form)
+        const name = formData.get('name')
+        const price = Number(formData.get('price'))
+        const variant = formData.get('variant')
+
+        if (!name || !name.toString().replaceAll(' ', '')) return handleError('Name product is required')
+        if (!price || isNaN(price) || price < 0) return handleError('Enter a valid number of products')
+
+        console.log(name, price)
+        // TODO => hacer fetch para crear producto
+        // Enviar name y price
+    }
+
+    return (
+        <Form
+            onSubmit={createProduct}
+            inputs={registerProductInputs}
+            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+            submitText="Create Product">
+            <h3>Register new product</h3>
+            {!!error && (<p>{error}</p>)}
+        </Form>)
 }
---
+++ src/Pages/Admin/Sections/ProductCreation/registerProductInputs.ts
@@ -0,0 +1,7 @@
+import { ForminputDataType } from "../../../../Models";
+
+export const registerProductInputs: ForminputDataType[] = [
+    { type: 'text', name: 'name', required: true },
+    { type: 'number', name: 'price', required: true },
+    { type: 'text', name: 'variant', description: 'separate with "," for more than one variant', required: true }
+]
