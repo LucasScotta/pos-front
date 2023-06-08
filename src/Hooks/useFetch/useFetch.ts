77a33859948a8d3a6356aab9f2b3306ca276155b
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-08 09:46:18 -0300

    useFetch hook created

--- src/Hooks/useFetch/useFetch.ts
+++ src/Hooks/useFetch/useFetch.ts
@@ -5,10 +5,14 @@ import { AxiosMethod } from "../../Models/AxiosMethod"
 export const useFetch = (url: string, method: AxiosMethod, data: any = {}) => {
     const [resp, setResp] = useState<AxiosResponse>()
     const [error, setError] = useState<Error>()
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         axios[method](url, data)
             .then(resp => setResp(resp))
             .catch(e => setError(e))
+            .finally(() => setLoading(false))
     }, [])
-    return { resp, error }
+
+    return { resp, error, loading }
 }
