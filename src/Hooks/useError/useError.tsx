b1d485dcc7be55dbbca4d3563c335f93d1eba825
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-10 18:40:09 -0300

    useError hook created

---
+++ src/Hooks/useError/useError.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+interface ErrorHookProps {
+    errorMessage?: string
+}
+
+export const useError = ({ errorMessage }: ErrorHookProps) => {
+    const [error, setError] = useState(errorMessage)
+    const [timeError, setTimeError] = useState<NodeJS.Timeout>()
+
+    const setErrorMessage = (message: string) => {
+        clearInterval(timeError)
+        setError(message)
+        setTimeError(setTimeout(() => setError(''), 3000))
+    }
+    useEffect(() => {
+        if (errorMessage) return setErrorMessage(errorMessage)
+    }, [])
+    return { error, setErrorMessage }
+}
