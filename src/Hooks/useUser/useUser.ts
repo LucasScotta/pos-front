f8786064b5f547e051fc38e122a3ee0f4e2f5cde
LucasScotta <lucasmanuelscotta@gmail.com>
2023-06-11 04:38:44 -0300

    clear user added to useUser custom hook

--- src/Hooks/useUser/useUser.ts
+++ src/Hooks/useUser/useUser.ts
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { AppStore, UserInfo } from "../../Models"
 import { useDispatch } from "react-redux"
+import { EmptyUserState } from "../../Provider/states/user"
 
 export const useUser = () => {
     const userState = useSelector((store: AppStore) => store.user)
     const dispatch = useDispatch()
 
-    const [user, setUser] = useState<UserInfo>()
+    const [user, setUser] = useState<UserInfo>(EmptyUserState)
     const updatedUser = () => setUser(userState)
-    useEffect(updatedUser, [])
     useEffect(updatedUser, [userState])
 
     return { user, dispatch }
